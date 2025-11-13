cf015faf37c8e5e1f4c1aa162ac760b6055b64f6
hassanmalik98333-cyber <hassanmalik98333@gmail.com>
2025-11-12 22:50:42 -0500

    todo list project done

--- 8.4(todo list exercise)/ToDo(localStorage).js
+++ 8.4(todo list exercise)/ToDo(localStorage).js
@@ -10,27 +10,16 @@ todoArray.forEach(todo => { // same as todoArray.forEach(function(todo) {...
     const newTodoElement = document.createElement('li');
     const removeTodo = document.createElement('button');
     removeTodo.classList.add('removeButton');
-    newTodoElement.innerText = todo.completed ? '' : todo.task;
-      let targetId = todo.id;
+    //to take the class from localStorage
+    if(todo.done === true){
+      newTodoElement.classList.add('done');
+    };
+    newTodoElement.innerText = todo.task;
+    newTodoElement.id = todo.id;//this adds the id to the element on page refresh.
       removeTodo.innerText ='X';
      newTodoElement.appendChild(document.createTextNode(' '));
     newTodoElement.appendChild(removeTodo);
     todoList.appendChild(newTodoElement);
-
-    todoList.addEventListener('click', function(e){
-        // if(e.target.tagName==='LI'){
-          
-   
-      if(e.target.tagName==='BUTTON'){
-        let newTodo = todoArray.filter(todo => todo.id !== targetId);
-         localStorage.setItem('todoList', JSON.stringify(newTodo));
-           
-
-    
-    
-  };
-   
-});
 }); 
 
 
@@ -44,9 +33,9 @@ form.addEventListener('submit',function(e){
     let inputValue = input.value.trim();
     if(inputValue !==''){
       const newTodo = {
-          id: Date.now(), //will a different different code(each it unique) for every object
+          id: Date.now(), //will give a different code(each code is unique) for every object
          task: inputValue,
-         completed: false // once the task is completed, you will turn it to true.
+         done: false
       };
     todoArray.push(newTodo);
     localStorage.setItem('todoList', JSON.stringify(todoArray));//
@@ -57,6 +46,7 @@ form.addEventListener('submit',function(e){
     const removeTodo = document.createElement('button');
     removeTodo.classList.add('removeButton');
     newTodoElement.innerText = inputValue;
+    newTodoElement.id = newTodo.id; // this adds the id to the element right away without needing to refresh.
     removeTodo.innerText ='X';
      newTodoElement.appendChild(document.createTextNode(' '));
     newTodoElement.appendChild(removeTodo);
@@ -70,8 +60,35 @@ form.addEventListener('submit',function(e){
     function listClicked(e){
       if(e.target.tagName==='LI'){
     e.target.classList.toggle('done');
+    //to save the class in localstorage
+     let identity = e.target.id;
+     let foundTodo = todoArray.find(todo => todo.id === parseInt(identity));//this isolates the object with the id
+     if(foundTodo){// this checks if the isolated object exists and if it exists, it runs the code bellow.
+       if(foundTodo.done === false){//for some reason === works here but not below, bellow only = works.
+        foundTodo.done = true;// only one = works here not ===
+       } else if(foundTodo.done === true){
+        foundTodo.done = false;
+       };
+      };// at the end of this code, the isolatd objectis updates and todoArray is updated.
+      
+       localStorage.setItem('todoList', JSON.stringify(todoArray));//this saves the updated todoArray into          local storage.
+     
+      console.log(foundTodo);
+
+    
+    
+  
+
 } else if(e.target.tagName==='BUTTON'){
     e.target.parentElement.remove();
+    let identity2 = e.target.parentElement.id;
+     let newTodoArray2 = todoArray.filter(todo => todo.id !== parseInt(identity2));
+     todoArray = newTodoArray2;
+         localStorage.setItem('todoList', JSON.stringify(todoArray));
+         console.log('localStorage.todoList: ',localStorage.todoList);
+ console.log('todoArray: ',todoArray);
+
+           
     
     
   };
